219c3fe01b75b37bd21badf734504b7cbbcad696
LEO <sardor020516@gmail.com>
2026-09-22 06:20:02 +0900

    fix: L-task

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+
+
+
+function reverseSentence(str: string): string {
+    return str
+        .split(" ")
+        .map((word: string) =>
+            word.replace(
+                /[a-zA-Z]+/g,
+                (letters: string) => letters.split("").reverse().join("")
+            )
+        )
+        .join(" ");
+}
+
+console.log(reverseSentence("we like coding!"));
