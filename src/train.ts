0a66ba520cfbd887939f7628e1012d1ad0aeef7e
LEO <sardor020516@gmail.com>
2026-09-24 02:35:26 +0900

    feat: M-task

--- src/train.ts
+++ src/train.ts
@@ -2,16 +2,30 @@
 
 
 
-function reverseSentence(str: string): string {
-    return str
-        .split(" ")
-        .map((word: string) =>
-            word.replace(
-                /[a-zA-Z]+/g,
-                (letters: string) => letters.split("").reverse().join("")
-            )
-        )
-        .join(" ");
+// function reverseSentence(str: string): string {
+//     return str
+//         .split(" ")
+//         .map((word: string) =>
+//             word.replace(
+//                 /[a-zA-Z]+/g,
+//                 (letters: string) => letters.split("").reverse().join("")
+//             )
+//         )
+//         .join(" ");
+// }
+
+// console.log(reverseSentence("we like coding!"));
+
+
+
+
+function getSquareNumbers(numbers: number[]) {
+    return numbers.map(function (number: number) {
+        return {
+            number: number,
+            square: number * number
+        };
+    });
 }
 
-console.log(reverseSentence("we like coding!"));
+console.log(getSquareNumbers([1, 2, 3]));
