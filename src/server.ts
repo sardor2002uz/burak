6bb392e1090dd0e5e0cdf414dad3d07bf9a58c33
LEO <sardor020516@gmail.com>
2026-09-24 01:45:08 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -7,3 +7,12 @@ console.log("MONGO_URL:", process.env.MONGO_URL);
 
 
 
+import mongoose from "mongoose";
+
+mongoose
+    .connect(process.env.MONGO_URL as string, {})
+    .then((data) => {
+        console.log("MongoDB connection succeed");
+        const PORT = process.env.PORT ?? 3003;
+    })
+    .catch((err) => console.log("ERROR on connection MongoDB", err));
